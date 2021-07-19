902f599ce7abf27dff3357e0b772730ec6387b13
Nelson Cortes <nelsondmondragon@gmail.com>
2021-07-19 10:55:12 -0500

    Uso de properties con ejemplo de generacion de POJO

--- src/main/java/com/fundamentals/springboot/fundamentals/FundamentalsApplication.java
+++ src/main/java/com/fundamentals/springboot/fundamentals/FundamentalsApplication.java
@@ -4,6 +4,7 @@ import com.fundamentals.springboot.fundamentals.bean.MyBean;
 import com.fundamentals.springboot.fundamentals.bean.MyBeanWithDependency;
 import com.fundamentals.springboot.fundamentals.bean.MyBeanWithProperties;
 import com.fundamentals.springboot.fundamentals.component.ComponentDependency;
+import com.fundamentals.springboot.fundamentals.pojo.UserPojo;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -16,14 +17,17 @@ public class FundamentalsApplication implements CommandLineRunner {
     private MyBean myBean;
     private MyBeanWithDependency myBeanWithDependency;
     private MyBeanWithProperties myBeanWithProperties;
+    private UserPojo userPojo;
 
 
     public FundamentalsApplication(@Qualifier("componentTwoImplement") ComponentDependency componentDependency
-            , MyBean myBean, MyBeanWithDependency myBeanWithDependency, MyBeanWithProperties myBeanWithProperties) {
+            , MyBean myBean, MyBeanWithDependency myBeanWithDependency, MyBeanWithProperties myBeanWithProperties
+            , UserPojo userPojo) {
         this.componentDependency = componentDependency;
         this.myBean = myBean;
         this.myBeanWithDependency = myBeanWithDependency;
         this.myBeanWithProperties = myBeanWithProperties;
+        this.userPojo = userPojo;
     }
 
     public static void main(String[] args) {
@@ -37,5 +41,6 @@ public class FundamentalsApplication implements CommandLineRunner {
         myBean.print();
         myBeanWithDependency.printWithDependency();
         System.out.println(myBeanWithProperties.function());
+        System.out.println(userPojo.getEmail() + "-" + userPojo.getPassword() + "-" + userPojo.getAge());
     }
 }
--- src/main/java/com/fundamentals/springboot/fundamentals/configuration/GeneralConfiguration.java
+++ src/main/java/com/fundamentals/springboot/fundamentals/configuration/GeneralConfiguration.java
@@ -2,11 +2,14 @@ package com.fundamentals.springboot.fundamentals.configuration;
 
 import com.fundamentals.springboot.fundamentals.bean.MyBeanPropertiesImplement;
 import com.fundamentals.springboot.fundamentals.bean.MyBeanWithProperties;
+import com.fundamentals.springboot.fundamentals.pojo.UserPojo;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableConfigurationProperties(UserPojo.class)
 public class GeneralConfiguration {
     @Value("${value.name}")
     private String name;
